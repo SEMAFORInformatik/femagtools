4cde5357d4da3fca01a22e4edbe3ffc67402be18
Ronald Tanner <tar@semafor.ch>
2016-11-12 15:09:46 +0100

    femagtools/losscoeffs.py

--- femagtools/vbf.py
+++ femagtools/vbf.py
@@ -9,17 +9,8 @@
 """
 import sys
 import numpy as np
+import femagtools.losscoeffs
 
-
-def findNotNone(l):
-    """return lower and upper indexes of not none values in list"""
-    for i in range(len(l)):
-        if l[i]:
-            break
-    for j in range(len(l)-1, -1, -1):
-        if l[j]:
-            break
-    return (i, j)
 
 fo = 50.
 Bo = 1.5
@@ -44,7 +35,7 @@ class Reader:
         with open(filename) as f:
             self.vbf['name'] = f.readline().strip()
             self.vbf['fo'], self.vbf['Bo'] = [float(s)
-                                             for s in f.readline().strip().split()]
+                                              for s in f.readline().strip().split()]
             # Ignore the next line
             f.readline()
             self.vbf['f'] = [float(s) for s in f.readline().strip().split()]
@@ -59,51 +50,7 @@ class Reader:
             
     def getLossValues(self):
         return self.vbf
-
-    def fitAllCoeffs(self):
-        betacoeffs = []
-        # fit beta: pfe = cw_b*(B/Bo)**beta
-        for i, fref in enumerate(self.vbf['f']):
-            if fref > 0:
-                pfe = np.array(self.vbf['pfe']).T[i]
-                j, k = findNotNone(pfe)
-                if j <= k:
-                    y = [np.log10(p) for p in pfe[j:k+1]]
-                    x = [np.log10(b/self.vbf['Bo'])
-                         for b in self.vbf['B'][j:k+1]]
-                    A = np.vstack([x, np.ones(len(x))]).T
-                    beta, w = np.linalg.lstsq(A, y)[0]
-                    betacoeffs.append(beta)
-
-        # fit alfa: pfe = cw_f*(f/fo)**alfa
-        alfacoeffs = []
-        for i in range(len(self.vbf['B'])):
-            pfe = np.array(self.vbf['pfe'])[i]
-            j, k = findNotNone(pfe)
-            if j <= k:
-                y = [np.log10(p) for p in pfe[j:k+1]]
-                x = [np.log10(f/self.vbf['fo']) for f in self.vbf['f'][j:k+1]]
-                A = np.vstack([x, np.ones(len(x))]).T
-                alfa, cw = np.linalg.lstsq(A, y)[0]
-                if alfa > 1.2 and alfa < 1.8:
-                    alfacoeffs.append(alfa)
-
-        if self.vbf['f']:
-            alfa = np.average(alfacoeffs)
-        else:
-            alfa = 1.3
-        beta = np.average(betacoeffs)
-        # fit cw: pfe = cw * (f/fo)**alfa * (B/Bo)**beta
-        cw = []
-        for i, f in enumerate(self.vbf['f']):
-            for k, B in enumerate(self.vbf['B']):
-                if i < len(self.vbf['pfe'][k]):
-                    pfe = self.vbf['pfe'][k][i]
-                    if pfe:
-                        a = (f/self.vbf['fo'])**alfa*(B/self.vbf['Bo'])**beta
-                        cw.append(pfe/a)
-        return(np.average(cw), alfa, beta)
-
+    
 if __name__ == "__main__":
     import matplotlib.pylab as pl
     if len(sys.argv) == 2:
@@ -113,15 +60,44 @@ if __name__ == "__main__":
             
     vbf = Reader(filename)
 
-    cw, alfa, beta = vbf.fitAllCoeffs()
+    print(vbf.getLossValues())
+    
+#    cw, alfa, beta = femagtools.losscoeffs.fitsteinmetz(
+#        vbf.vbf['f'],
+#        vbf.vbf['B'],
+#        vbf.vbf['pfe'],
+#        vbf.vbf['Bo'],
+#        vbf.vbf['fo'])
 
     n = 100
     B = pl.np.linspace(0.1, 2, n)
     
-    for i in range(len(vbf.vbf['f'])):
-        f = vbf.vbf['f'][i]
-        pfe = [p for p in np.array(vbf.vbf['pfe']).T[i] if p > 0]
-        pl.plot(B, pfe2(f, B, cw, alfa, beta))
+#    for i in range(len(vbf.vbf['f'])):
+#        f = vbf.vbf['f'][i]
+#        pfe = [p for p in np.array(vbf.vbf['pfe']).T[i] if p]
+#        pl.plot(B, pfe2(f, B, cw, alfa, beta))
+#        pl.plot(vbf. vbf['B'][:len(pfe)], pfe,
+#                marker='o', label="f1={} Hz".format(f))
+
+#    pl.title("Iron Losses " + filename)
+#    pl.yscale('log')
+#    pl.xscale('log')
+#    pl.xlabel("Induction [T]")
+#    pl.ylabel("Pfe [W/kg]")
+#    pl.grid(True)
+#    pl.show()
+
+    z = femagtools.losscoeffs.fitjordan(
+        vbf.vbf['f'],
+        vbf.vbf['B'],
+        vbf.vbf['pfe'],
+        vbf.vbf['Bo'],
+        vbf.vbf['fo'])
+
+    cw, ch = (z[0], z[1])
+    for i, f in enumerate(vbf.vbf['f']):
+        pfe = [p for p in np.array(vbf.vbf['pfe']).T[i] if p]
+        pl.plot(B, pfe1(f, B, ch, 1, cw, 2, 2))
         pl.plot(vbf. vbf['B'][:len(pfe)], pfe,
                 marker='o', label="f1={} Hz".format(f))
 

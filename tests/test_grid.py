c8371285e280d7c8731befc6229e8dd2f0ddfd83
Ronald Tanner <tar@semafor.ch>
2017-04-19 10:22:16 +0200

    added test

---
+++ tests/test_grid.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+import femagtools.grid
+
+
+def test_create_parameter_range():
+    x = [(1, 2, 3), (4, 5), (6, 7)]
+
+    r = femagtools.grid.create_parameter_range(x)
+
+    assert r.tolist() == [[1, 4, 6],
+                          [2, 4, 6],
+                          [3, 4, 6],
+                          [1, 5, 6],
+                          [2, 5, 6],
+                          [3, 5, 6],
+                          [1, 4, 7],
+                          [2, 4, 7],
+                          [3, 4, 7],
+                          [1, 5, 7],
+                          [2, 5, 7],
+                          [3, 5, 7]]
+
+

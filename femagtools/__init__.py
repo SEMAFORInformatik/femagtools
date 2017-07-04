271613b0db7c794aec5e4f4aa18f6bff150c2099
Ronald Tanner <tar@semafor.ch>
2017-07-04 11:07:02 +0200

    Rel 0.1

--- femagtools/__init__.py
+++ femagtools/__init__.py
@@ -9,7 +9,7 @@
     :license: BSD, see LICENSE for more details.
 """
 __title__ = 'femagtools'
-__version__ = '0.0.25'
+__version__ = '0.1'
 __author__ = 'Ronald Tanner'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2016 SEMAFOR Informatik & Energie AG'

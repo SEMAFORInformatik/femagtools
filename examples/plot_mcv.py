09e2cf410550c2a113578b814ff55dc831526c03
Ronald Tanner <tar@semafor.ch>
2017-09-12 15:25:14 +0200

    added examples

--- examples/plot_mcv.py
+++ examples/plot_mcv.py
@@ -16,32 +16,43 @@ mcv = femagtools.mcv.Reader()
 mcv.readMcv(sys.argv[1])
 r = mcv.get_results()
 
-bh = [(bi, hi)
-      for bi, hi in zip(r['curve'][0]['bi'],
-                        r['curve'][0]['hi']) if bi > 0 and hi > 0]
-
-ji = [b-MUE0*h for b, h in bh]
+if r['ctype'] in (femagtools.mcv.MAGCRV, femagtools.mcv.ORIENT_CRV):
+    ncols = 2
+    bh = [(bi, hi*1e-3)
+          for bi, hi in zip(r['curve'][0]['bi'],
+                            r['curve'][0]['hi']) if bi > 0 and hi > 0]
+    
+    ji = [b-MUE0*h*1e3 for b, h in bh]
+else:  # Permanent Magnet
+    ncols = 1
+    bh = [(bi, hi*1e-3)
+          for bi, hi in zip(r['curve'][0]['bi'],
+                            r['curve'][0]['hi'])]
+    ji = []
 bi, hi = zip(*bh)
 
-fig = pl.figure()
-g = pl.GridSpec(1, 2)
-ax = [pl.subplot(g[0, 0]),
-      pl.subplot(g[0, 1])]
-ax[0].set_title(r['name'])
-ax[0].semilogx(hi, bi, label='Induction')
-ax[0].semilogx(hi, ji, label='Polarisation')
-ax[0].set_xlabel('H / A/m')
-ax[0].set_ylabel('T')
-ax[0].legend(loc='lower right')
-ax[0].grid()
-    
-ur = [bx/hx/MUE0 for bx, hx in bh]
-ax[1].plot(bi, ur)
-ax[1].set_xlabel('B / T')
-ax[1].set_title('Muer')
-ax[1].grid()
+fig, ax = pl.subplots(nrows=1, ncols=ncols)
+if ncols > 1:
+    ax[0].set_title(r['name'])
+    ax[0].semilogx(hi, bi, label='Induction')
+    ax[0].semilogx(hi, ji, label='Polarisation')
+    ax[0].set_xlabel('H / kA/m')
+    ax[0].set_ylabel('T')
+    ax[0].legend(loc='lower right')
+    ax[0].grid()
+
+    ur = [bx/hx/MUE0*1e-3 for bx, hx in bh]
+    ax[1].plot(bi, ur)
+    ax[1].set_xlabel('B / T')
+    ax[1].set_title('rel. Permeability')
+    ax[1].grid()
+else:
+    ax.set_title(r['name'])
+    ax.plot(hi, bi, label='Induction')
+    ax.set_xlabel('H / kA/m')
+    ax.set_ylabel('T')
+    ax.grid()
     
 fig.tight_layout()
 fig.subplots_adjust(top=0.94)
 pl.show()
-

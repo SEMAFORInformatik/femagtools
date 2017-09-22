d8efeb99ffa198a00951fed30bfcb2172e49b03a
Ronald Tanner <tar@semafor.ch>
2017-09-22 14:48:08 +0200

    prep rel 0.2.7 (conda deploy)

--- conda_deploy.sh
+++ conda_deploy.sh
@@ -11,11 +11,12 @@ echo "Create skeleton.."
 conda skeleton pypi femagtools
 
 echo "Build package.."
-dest_dir=`conda build femagtools --output`
+dest_file=$(conda build femagtools --output)
+dest_dir=$(dirname $dest_file)
 echo "File located under $dest_dir"
 
 echo "Converting conda package.."
-conda convert -f --platform all $dest_dir -o builds
+conda convert -f --platform all $dest_dir/femagtools-*.tar.bz2 -o builds
 
 echo "Deploying..."
 anaconda -t $ANACONDA_TOKEN upload builds/**/femagtools-*.tar.bz2

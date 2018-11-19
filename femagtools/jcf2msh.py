d680655d27bdd6c0764787eba6b16a9a39e13282
Jonas Knöpfel <knj@semafor.ch>
2018-11-19 12:29:10 +0100

    remove duplicate nodes in airgap

--- femagtools/jcf2msh.py
+++ femagtools/jcf2msh.py
@@ -11,7 +11,7 @@
 import argparse
 import re
 from zipfile import ZipFile
-from collections import defaultdict
+from collections import namedtuple
 
 parser = argparse.ArgumentParser()
 parser.add_argument("infile")
@@ -24,48 +24,76 @@ outfile = args.outfile
 
 with ZipFile(infile, 'r') as jcf:
     with jcf.open("mesh.dat") as f:
-        stuff = [l.decode("utf-8") for l in f.readlines()]
-    
-mesh = {"Node": [], "Element": []}
+        f = [l.decode("utf-8") for l in f.readlines()]
+        f = iter(f)
+        
+def next_line(f, n=1):
+    for i in range(n):
+        line = next(f)
+    line = line.strip()
+    line = re.split(" +|,", line)
+    return line
 
-def get_line():
-    return re.split(" +|\n+|\*", stuff[i])
-    
-i = 0
-line = []
-while i < len(stuff):
-    line = get_line()
-    if line[-2] in ("Node", "Element"):
-        reading = line[-2]
-        i+=1
-        for el in range(int(line[1])):
-            line = get_line()
-            i+=1
-            mesh[reading].append(line)
-    else:
-        i+=1
 
-ele_type = {3: "2", 4: "3"}
+Node = namedtuple("Node", ["id", "x", "y"])
+nodes = []
 
-msh = []
-msh.append("$MeshFormat\n2.2 0 8\n$EndMeshFormat")
+Element = namedtuple("Element", ["nodes", "entity"])
+elements = []
+
+while True:
+    try:
+        line = next_line(f)
+        
+        if line[-1] == "*Node":
+            for n in range(int(line[0])):
+                line = next_line(f)
+                nodes.append(Node(line[1], line[2], line[3]))
+                
+            replace = {}
+            for i, n1 in enumerate(nodes):
+                for n2 in nodes[i+1:]:
+                    if n1.x == n2.x and n1.y == n2.y and n1.id != n2.id:
+                        if n1.id not in replace:
+                            replace[n2.id] = n1.id
+                            
+            nodes = [n for n in nodes if n not in replace]
+                
+        if line[-1] == "*Element":
+            for n in range(int(line[0])):
+                line = next_line(f)
+                if line[1] == "3":
+                    elements.append(
+                        Element([line[-3], line[-2], line[-1], None], line[2]))
+                elif line[1] == "4":
+                    elements.append(
+                        Element([line[-4], line[-3], line[-2], line[-1]], line[2]))
+                    
+            for e in elements:
+                for i, n in enumerate(e.nodes):
+                    if n in replace:
+                        e.nodes[i] = replace[n]
+                    
+    except StopIteration:
+        break
+
+msh = ["$MeshFormat\n2.2 0 8\n$EndMeshFormat"]
+
 msh.append("$Nodes")
-msh.append(str(len(mesh["Node"])))
-
-for n in mesh["Node"]:
-    msh.append(" ".join([n[2], n[3], n[4], "0.0"]))
+msh.append(str(len(nodes)))
+for i, n in enumerate(nodes):
+    msh.append("{} {} {} 0.0".format(n.id, n.x, n.y))
 msh.append("$EndNodes")
 
 msh.append("$Elements")
-msh.append(str(len(mesh["Element"])))
-
-for e in mesh["Element"]:
-    e_id = e[3]
-    num_v = int(e[2])
-    n_ids = []
-    for v in reversed(range(num_v)):
-        n_ids.append(e[-(v+2)])
-    msh.append(" ".join([e[1], ele_type[num_v], "2", "0", str(e_id)] + n_ids))
+msh.append(str(len(elements)))
+for i, e in enumerate(elements):
+    if e.nodes[3]:
+        msh.append("{} 3 2 0 {} {} {} {} {}".format(
+            i + 1, e.entity, e.nodes[0], e.nodes[1], e.nodes[2], e.nodes[3]))
+    else:
+        msh.append("{} 2 2 0 {} {} {} {}".format(
+            i + 1, e.entity, e.nodes[0], e.nodes[1], e.nodes[2]))
 msh.append("$EndElements")
                
 with open(outfile, "w") as outfile:

597495a54e350f34552b35ef529ce81e570bd100
Ronald Tanner <tar@semafor.ch>
2019-03-22 11:07:00 +0100

    agndst from angles

--- femagtools/dxfsl/fslrenderer.py
+++ femagtools/dxfsl/fslrenderer.py
@@ -26,16 +26,28 @@ def lcm(a, b):
     return a*b//gcd(a, b)
 
     
-def agndst(da1, da2, Q, p):
-    """return agndst"""
+def agndst2(da1, da2, Q, p):
+    """return agndst from lcm"""
     r = (da1 + da2)/4
     ag = abs(da1 - da2)/6
     n = 12
-    a = [r*4*np.pi/p/(i*lcm(Q, p)) for i in range(1, n)]
+    a = [r*4*np.pi/p/(i*lcm(Q, p))
+         for i in range(1, n)]
     i = np.argmin(np.abs(np.array([ag]*(n-1)) - a))
     if i > 0:
         return a[i-1]
     return a[0]
+
+
+def agndst(da1, da2, Q, p):
+    """ build agndst from set of useful node angles:  
+        4°, 2°, 1.5°, 1°, 0.75°, 0.5°, 0.25, 0.1, 0.05° """
+    dagset = [np.pi/45, np.pi/90, np.pi/120, np.pi/180,
+              np.pi/240, np.pi/360, np.pi/720, np.pi/1800, np.pi/3600]
+    r = (da1 + da2)/4
+    ag = abs(da1 - da2)/6
+    i = max(np.argmin(np.abs(np.array(dagset) - np.arctan2(ag, r))), 1)
+    return dagset[i-1]*r
 
 
 class FslRenderer(object):

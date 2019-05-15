df2891a7facb8a8d141a6d4f0db97be79d146c3e
Ronald Tanner <tar@semafor.ch>
2019-05-15 10:36:39 +0200

    fixed airgap flux density DFT

--- femagtools/airgap.py
+++ femagtools/airgap.py
@@ -8,65 +8,60 @@
 
 
 """
-import sys
 import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def read(filename, taup):
+def read(filename, pmod):
     """read dat file with columns (phi, Br, Bphi)
     returns samples, values, amplitude and phase of base harmonic
 
     Args:
       filename: the name of the file to be processed
-      taup: angle of a effective pole pair (urwicklung) in degrees
+      pmod: number of poles in model
     """
     bag = np.loadtxt(filename).T
     if len(bag) < 3:
         logger.warn("%s has incomplete content", filename)
         return(dict())
 
-    phi = bag[0]
-    nsamples=len(phi)
-    br = bag[1]
-    phisteps = [phi[i+1] - phi[i] for i in range(len(phi)-1)]
-    dphi = sum(phisteps)/len(phisteps)
+    N = 2**10  # The DFT is most efficient when N is a power of 2
+    phi = np.linspace(0, 2*np.pi, N)
+    nphi = int(360/(bag[0][1] - bag[0][0])) + 1
+    npoles = (nphi-1)//(len(bag[0]) - 1)
+    phitab = np.linspace(0, 2*np.pi, nphi)
+    if pmod % 2:
+        bx = np.append(
+            np.concatenate(
+                [n*bag[1][:-1]
+                 for n in [m % 2 or -1
+                           for m in range(1, npoles+1)]]),
+            bag[1][0])
+    else:
+        bx = np.append(
+            np.tile(bag[1][:-1], npoles),
+            bag[1][0])
 
-    if round(phi[-1]/taup) < 1:
-        br = np.append(br, -1*br)
+    br = np.interp(phi, phitab, bx)
         
-    freq = np.fft.fftfreq(len(br), d=dphi)
-    # compute FFT from induction
-    Y = 2*np.fft.rfft(br)/len(br)
-    
-    # find the peak and interpolate
-    i = np.argmax(abs(Y))
-    logger.debug("Len %d max phi %f max amp %f",
-                 len(phi), phi[-1], abs(Y[i]))
+    # compute DFT from induction
+    Y = np.fft.fft(br)
 
-    if freq[i] > 0:
-        logger.info("%s: Period %f max phi %f B amp %f",
-                    filename, 1/freq[i], len(br)*dphi, abs(Y[i]))
-        a = abs(Y[i])
-        T0 = abs(1/freq[i])
-        x0 = np.angle(Y[i])
-        phi = [ix*dphi for ix in range(len(br))]
-        x = np.array([2*x/T0*np.pi for x in phi])
+    # find the peak (amplitude of base harmonic)
+    i = np.argmax(np.abs(Y))
+    a = 2*np.abs(Y[i])/N
+    logger.info("%s: B amp %f",
+                filename, a)
+    
+    alfa0 = np.angle(Y[i])
+    alfa = npoles/2*bag[0]/180*np.pi
 
     return dict(Bamp=a,
-                phi0=x0,
-                pos=phi[:nsamples],
-                B=br.tolist()[:nsamples],
-                B_fft=(a*np.cos(x+x0)[:nsamples]).tolist())
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s %(message)s')
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-    else:
-        filename = sys.stdin.readline().strip()
-            
-    print(read(filename)['Bamp'])
+                phi0=alfa0,
+                pos=bag[0],
+                B=bag[1],
+                nue=np.arange(0, 9*npoles),
+                B_nue=2*np.abs(Y[:9*npoles])/N,
+                B_fft=(a*np.cos(alfa+alfa0)).tolist())
--- tests/test_airgap_induction.py
+++ tests/test_airgap_induction.py
@@ -9,7 +9,5 @@ def test_airgap_induction():
     testPath = os.path.join(os.path.split(__file__)[0], 'data')
     if len(testPath) == 0:
         testPath = os.path.join(os.path.abspath('.'), 'data')
-    r = ag.read(os.path.join(testPath, 'bag.dat'), 90)
-    # FIXME: Python2 1.1698
-    #np.testing.assert_almost_equal(r['Bamp'], 1.271, 3)
-    assert 1.16 < r['Bamp'] < 1.28
+    r = ag.read(os.path.join(testPath, 'bag.dat'), 1)
+    np.testing.assert_almost_equal(r['Bamp'], 1.26914, 3)

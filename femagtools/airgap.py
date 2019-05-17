323be8691653e96b348ee91266d53410c03ce0bc
Ronald Tanner <tar@semafor.ch>
2019-05-17 10:58:42 +0200

    fixed flux dens fft calc

--- femagtools/airgap.py
+++ femagtools/airgap.py
@@ -30,38 +30,41 @@ def read(filename, pmod):
     N = 2**10  # The DFT is most efficient when N is a power of 2
     phi = np.linspace(0, 2*np.pi, N)
     nphi = int(360/(bag[0][1] - bag[0][0])) + 1
-    npoles = (nphi-1)//(len(bag[0]) - 1)
+    ntiles = (nphi-1)//(len(bag[0]) - 1)
     phitab = np.linspace(0, 2*np.pi, nphi)
     if pmod % 2:
         bx = np.append(
             np.concatenate(
                 [n*bag[1][:-1]
                  for n in [m % 2 or -1
-                           for m in range(1, npoles+1)]]),
+                           for m in range(1, ntiles+1)]]),
             bag[1][0])
     else:
         bx = np.append(
-            np.tile(bag[1][:-1], npoles),
+            np.tile(bag[1][:-1], ntiles),
             bag[1][0])
 
     br = np.interp(phi, phitab, bx)
-        
+    npoles = ntiles*pmod
+
     # compute DFT from induction
     Y = np.fft.fft(br)
-
-    # find the peak (amplitude of base harmonic)
-    i = np.argmax(np.abs(Y))
-    a = 2*np.abs(Y[i])/N
-    logger.info("%s: B amp %f",
-                filename, a)
+    freq = np.fft.fftfreq(N, d=phi[1]-phi[0])
     
+    # find the peak (amplitude of base harmonic)
+    i = np.argmax(np.abs(Y[:N//2]))
+    a = 2*np.abs(Y[i])/N
+    T0 = np.abs(1/freq[i])
+    logger.info("%s: %s poles B amp %f ",
+                filename, npoles, a)
+
     alfa0 = np.angle(Y[i])
-    alfa = npoles/2*bag[0]/180*np.pi
+    alfa = bag[0]/180*np.pi
 
     return dict(Bamp=a,
                 phi0=alfa0,
-                pos=bag[0],
-                B=bag[1],
-                nue=np.arange(0, 9*npoles),
-                B_nue=2*np.abs(Y[:9*npoles])/N,
-                B_fft=(a*np.cos(alfa+alfa0)).tolist())
+                pos=bag[0].tolist(),
+                B=bag[1].tolist(),
+                nue=np.arange(0, 9*npoles).tolist(),
+                B_nue=(2*np.abs(Y[:9*npoles])/N).tolist(),
+                B_fft=(a*np.cos(2*np.pi*alfa/T0+alfa0)).tolist())

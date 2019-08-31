c9efe8fe6ccf3e60c85fac90c33ff1a81794ad7d
Ronald Tanner <tar@semafor.ch>
2019-08-31 07:31:55 +0200

    added save_model

--- tests/test_fsl.py
+++ tests/test_fsl.py
@@ -312,7 +312,7 @@ class FslBuilderTest(unittest.TestCase):
         model = femagtools.MachineModel('data/magnsec')
         feapars['calculationMode'] = "cogg_calc"
         fsl = self.builder.create(model, feapars)
-        self.assertEqual(len(fsl), 82)
+        self.assertEqual(len(fsl), 83)
         
     def test_create_plots(self):
         pars = copy.deepcopy(feapars)

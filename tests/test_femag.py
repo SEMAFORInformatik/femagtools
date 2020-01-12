a8ac684a7c04d522a2b249f95f5795add473a238
Ronald Tanner <tar@semafor.ch>
2020-01-12 16:08:12 +0100

    fixed python 2.7 bug

--- tests/test_femag.py
+++ tests/test_femag.py
@@ -5,7 +5,8 @@ def test_run_script(monkeypatch, tmpdir):
     def mock_run(*args, **kwargs):
         return
     monkeypatch.setattr(femagtools.femag.Femag, "run", mock_run)
-    femag = femagtools.femag.Femag(tmpdir)
+    femag = femagtools.femag.Femag(str(tmpdir))
     r = femag(dict(), dict())
     assert r['status'] == 'ok'
+    assert tmpdir.join("femag.fsl").exists()
   

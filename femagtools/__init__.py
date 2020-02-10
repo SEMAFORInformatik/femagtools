e4c1f4f20bc4717ce6203e28d7fc01129d838032
Ronald Tanner <tar@semafor.ch>
2020-02-10 07:42:43 +0100

    new dev cycle

--- femagtools/__init__.py
+++ femagtools/__init__.py
@@ -9,7 +9,7 @@
 
 """
 __title__ = 'femagtools'
-__version__ = '0.9.29'
+__version__ = '0.9.30i-snapshot'
 __author__ = 'Ronald Tanner'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2016-2018 SEMAFOR Informatik & Energie AG'

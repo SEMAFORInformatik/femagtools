9068fb3944f9246314d4e48ddf3f3de509a78649
Ronald Tanner <tar@semafor.ch>
2020-08-19 13:42:35 +0200

    reduced python versions 3.7, 3.8

--- setup.py
+++ setup.py
@@ -54,8 +54,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering'],

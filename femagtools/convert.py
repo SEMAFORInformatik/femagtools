3ce8d51ad19c6eb9d44da437e2df2ca29545c9c4
Ronald Tanner <tar@semafor.ch>
2020-11-04 11:35:09 +0100

    handle missing mag temp

--- femagtools/convert.py
+++ femagtools/convert.py
@@ -190,11 +190,15 @@ def _from_isa(isa, filename, target_format,
         elif len(ev) == 4:
             quads.append([n.key - 1 for n in ev])
             cell_type = "quad"
-            
+
+        try:
+            magtemp = isa.MAGN_TEMPERATURE
+        except AttributeError:
+            magtemp = 20
         physical_ids[cell_type].append(physical_surface(e))
         geometrical_ids[cell_type].append(e.se_key)
         b[cell_type].append(e.induction())
-        h[cell_type].append(e.demagnetization(isa.MAGN_TEMPERATURE))
+        h[cell_type].append(e.demagnetization(magtemp))
         perm[cell_type].append(e.permeability())
         iron_losses[cell_type].append(e.iron_loss_density())
         mag_losses[cell_type].append(e.mag_loss_density())

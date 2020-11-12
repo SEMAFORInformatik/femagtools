297aa884c0e2522c154e941921d45ae900b9d1b6
J. Germishuizen <johannes.germishuizen@siemens.com>
2020-11-12 11:40:09 +0100

    Alternative DFT

--- femagtools/airgap.py
+++ femagtools/airgap.py
@@ -10,6 +10,8 @@
 """
 import numpy as np
 import logging
+import math
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -20,13 +22,47 @@ def read(filename, pmod):
 
     Args:
       filename: the name of the file to be processed
-      pmod: number of poles in model
+      pmod:     number of poles in model
     """
     bag = np.loadtxt(filename).T
     if len(bag) < 3:
         logger.warn("%s has incomplete content", filename)
         return(dict())
 
+    time1 = time.time()
+    model_angle = bag[0][-1]-bag[0][0]
+    pole_pitch = model_angle/pmod
+    pole_pairs = int(math.ceil(360.0/pole_pitch-0.5)/2)
+    per_bnd = math.ceil(model_angle/pole_pitch-0.5) % 2
+    n_copy = math.ceil(360.0/model_angle-0.5)
+    
+    bi = []    
+    xi = []
+    if per_bnd:
+        ''' 
+          Periodicity of the model:
+          (a) per_bnd = 0 the periodicity is positiv
+          (b) per_bnd = 1 the periodicity is negativ
+        '''
+        logger.info("negative periodicity")
+        for i in range(n_copy):
+            bi = np.append(bi,(-1)**(i)*bag[1][:-1])
+            xi = np.append(xi,bag[0][-1]*(i)+bag[0][:-1])
+    else:
+        logger.info("positive periodicity")
+        for i in range(n_copy):
+            bi = np.append(bi,bag[1][:-1])
+            xi = np.append(xi,bag[0][-1]*(i)+bag[0][:-1])
+            
+
+    Y = np.fft.fft(bi[0:-1])
+    a = 2*np.abs(Y[pole_pairs])/(len(bi[0:-1]))
+    time2 = time.time()
+    logger.info("%s: %d pole pairs B amp %f Dauer der Berechnung %f",
+                filename, pole_pairs, a, (time2-time1)*1000.0)
+    
+
+    time3 = time.time()
     N = 2**10  # The DFT is most efficient when N is a power of 2
     phi = np.linspace(0, 2*np.pi, N)
     nphi = int(round(360/((bag[0][-1] - bag[0][0])/(len(bag[0]) - 1)))) + 1
@@ -35,8 +71,8 @@ def read(filename, pmod):
     if pmod % 2:
         bx = np.append(
             np.concatenate(
-                [n*bag[1][:-1]
-                 for n in [m % 2 or -1
+                [n*bag[1][:-1] 
+                 for n in [m % 2 or -1 
                            for m in range(1, ntiles+1)]]),
             bag[1][0])
     else:
@@ -55,11 +91,18 @@ def read(filename, pmod):
     i = np.argmax(np.abs(Y[:N//2]))
     a = 2*np.abs(Y[i])/N
     T0 = np.abs(1/freq[i])
-    logger.info("%s: %s poles B amp %f ",
-                filename, npoles, a)
-
+    time4 = time.time()
+    logger.info("%s: %s poles B amp %f Dauer der Berechnung %f",
+                filename, npoles, a, (time4-time3)*1000.0)
+    
     alfa0 = np.angle(Y[i])
     alfa = bag[0]/180*np.pi
+    
+    logger.info("model angle: %f ",model_angle)
+    logger.info("pole_pitch: %f ",pole_pitch)
+    logger.info("number of copies: %d ",n_copy)
+    logger.info("periodicity: %d ",per_bnd)
+    logger.info("pole pairs: %d ",pole_pairs)    
 
     return dict(Bamp=a,
                 phi0=alfa0,
@@ -67,4 +110,6 @@ def read(filename, pmod):
                 B=bag[1].tolist(),
                 nue=np.arange(0, 9*npoles).tolist(),
                 B_nue=(2*np.abs(Y[:9*npoles])/N).tolist(),
-                B_fft=(a*np.cos(2*np.pi*alfa/T0+alfa0)).tolist())
+                B_fft=(a*np.cos(2*np.pi*alfa/T0+alfa0)).tolist(),
+                BX = bi.tolist(),
+                XX = xi.tolist())

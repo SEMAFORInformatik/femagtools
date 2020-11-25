8d49bb5ac08081d1e34d5c4074349c1b7916dbf3
Ronald Tanner <tar@semafor.ch>
2020-11-25 09:54:35 +0100

    fixed test

--- tests/test_tksreader.py
+++ tests/test_tksreader.py
@@ -15,7 +15,7 @@ def test_read_tks():
     assert len(tks['losses']['B']) == 13
     assert len(tks['losses']['pfe']) == 4
     assert len(tks['losses']['pfe'][0]) == 13
-    assert tks['losses']['f'][-1] == 400.0
+    assert tks['losses']['f'] == [50.0, 100.0, 200.0, 400.0]
     assert round(tks['losses']['B'][-1], 1) == 1.8
     # 13.1279112, 17.92205587, 23.79869172, 30.84129721, 39.3434623, 48.97389844, 60.25629981, 72.88007909
     assert len([x for x in tks['losses']['pfe'][3] if x]) == 9

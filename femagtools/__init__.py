2b8c957ea53a85d1d3bd96d56a3c84337725987d
Ronald Tanner <tar@semafor.ch>
2021-04-28 20:43:30 +0200

    PEP 440

--- femagtools/__init__.py
+++ femagtools/__init__.py
@@ -9,7 +9,7 @@
 
 """
 __title__ = 'femagtools'
-__version__ = '1.0.8-snapshot'
+__version__ = '1.0.8.dev1'
 __author__ = 'Ronald Tanner'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2016-2018 SEMAFOR Informatik & Energie AG'

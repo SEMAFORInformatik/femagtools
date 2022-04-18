723741a4d6d658da0c6ae7fa1e3967873145ba39
Ronald Tanner <tar@semafor.ch>
2022-04-18 17:07:15 +0200

    fixed test errors

--- tests/test_ts.py
+++ tests/test_ts.py
@@ -10,21 +10,36 @@ def losses():
 
 
 def test_read(losses):
-    assert [{'key': 1, 'name': 'stfe', 'losses': 0.0},
-            {'key': 2, 'name': 'rofe', 'losses': 0.0},
-            {'key': 3, 'name': 'wefe', 'losses': 0.0},
-            {'key': 4, 'name': '', 'losses': 0.001912685411090982},
-            {'key': 5, 'name': '', 'losses': 0.0019698298472865085},
-            {'key': 6, 'name': '', 'losses': 0.0018862924108952414}] == losses.ohm_lossenergy(0.0, 0.0)
-    assert [{'key': 1, 'name': 'stfe', 'losses': 0.0},
-            {'key': 2, 'name': 'rofe', 'losses': 0.0},
-            {'key': 3, 'name': 'wefe', 'losses': 0.0},
-            {'key': 4, 'name': '', 'losses': 0.09808646181380988},
-            {'key': 5, 'name': '', 'losses': 0.10101694663178479},
-            {'key': 6, 'name': '', 'losses': 0.09673297420375133}] == losses.ohm_powerlosses()
-    assert [{'key': 1, 'name': 'stfe', 'losses': 0.0},
-            {'key': 2, 'name': 'rofe', 'losses': 0.0},
-            {'key': 3, 'name': 'wefe', 'losses': 0.0},
-            {'key': 4, 'name': '', 'losses': 0.10108592471259238},
-            {'key': 5, 'name': '', 'losses': 0.10108593475500291},
-            {'key': 6, 'name': '', 'losses': 0.10108575785653288}] == losses.ohm_powerlosses_fft()
+    e = losses.ohm_lossenergy(0.0, 0.0)
+    assert len(e) == 6
+    assert {'key': 1, 'name': 'stfe', 'losses': 0.0} == e[0]
+    assert {'key': 2, 'name': 'rofe', 'losses': 0.0} == e[1]
+    assert {'key': 3, 'name': 'wefe', 'losses': 0.0} == e[2]
+    assert pytest.approx(
+        {'key': 4, 'name': '', 'losses': 0.001913}, abs=1e-5) == e[3]
+    assert pytest.approx(
+        {'key': 5, 'name': '', 'losses': 0.001967}, abs=1e-5) == e[4]
+    assert pytest.approx(
+        {'key': 6, 'name': '', 'losses': 0.001886}, abs=1e-5) == e[5]
+    p = losses.ohm_powerlosses()
+    assert len(p) == 6
+    assert {'key': 1, 'name': 'stfe', 'losses': 0.0} == p[0]
+    assert {'key': 2, 'name': 'rofe', 'losses': 0.0} == p[1]
+    assert {'key': 3, 'name': 'wefe', 'losses': 0.0} == p[2]
+    assert pytest.approx(
+        {'key': 4, 'name': '', 'losses': 0.09809}, abs=1e-5) == p[3]
+    assert pytest.approx(
+        {'key': 5, 'name': '', 'losses': 0.10102}, abs=1e-5) == p[4]
+    assert pytest.approx(
+        {'key': 6, 'name': '', 'losses': 0.09673}, abs=1e-5) == p[5]
+    p = losses.ohm_powerlosses_fft()
+    assert len(p) == 6
+    assert {'key': 1, 'name': 'stfe', 'losses': 0.0} == p[0]
+    assert {'key': 2, 'name': 'rofe', 'losses': 0.0} == p[1]
+    assert {'key': 3, 'name': 'wefe', 'losses': 0.0} == p[2]
+    assert pytest.approx(
+        {'key': 4, 'name': '', 'losses': 0.101086}, abs=1e-5) == p[3]
+    assert pytest.approx(
+        {'key': 5, 'name': '', 'losses': 0.101086}, abs=1e-5) == p[4]
+    assert pytest.approx(
+        {'key': 6, 'name': '', 'losses': 0.101086}, abs=1e-5) == p[5]

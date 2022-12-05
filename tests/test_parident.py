956a4f20362660a3f39f6d6a18433a7765f5f4cd
Ronald Tanner <tar@semafor.ch>
2022-12-05 20:47:37 +0100

    fixed test

--- tests/test_parident.py
+++ tests/test_parident.py
@@ -1,3 +1,4 @@
+import numpy as np
 import femagtools.machine
 from femagtools.job import Task
 import pathlib
@@ -21,3 +22,18 @@ def test_ecsim_eval(data_dir):
     assert expected['ls2'] == r['ls2']
     assert expected['zeta2'] == round(r['zeta2'], 4)
     assert expected['pl2v'] == round(r['pl2v'], 3)
+
+
+def test_noloadrot_eval(data_dir):
+    task = Task(id=1, directory=data_dir / 'parident')
+    i1tab = [2.0384197650236815, 4.076839530047363,
+             6.115259295071045, 8.153679060094726, 10.192098825118407]
+    u1ph = 400/np.sqrt(3)
+    eval = femagtools.machine.im._eval_noloadrot(i1tab, u1ph)
+    r = eval(task)
+    i0 = [2.0384,  4.0768,  6.1153,  8.1537, 10.1921]
+    psi1_0 = [0.2725, 0.5301, 0.7106, 0.8091, 0.8641]
+    Bamp = [0.3091, 0.6013, 0.8058, 0.9159, 0.9769]
+    assert i0 == [round(x, 4) for x in r['i1_0']]
+    assert psi1_0 == [round(x, 4) for x in np.mean(r['psi1_0'], axis=1)]
+    assert Bamp == [round(x, 4) for x in np.mean(r['Bamp'], axis=1)]

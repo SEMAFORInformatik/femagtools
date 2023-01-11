06d626a5e13f9940fff65a18fa39a168e2cd217d
Ronald Tanner <tar@semafor.ch>
2023-01-11 14:00:20 +0100

    code formatting, removed chmod

--- tests/test_amela.py
+++ tests/test_amela.py
@@ -1,28 +1,27 @@
-from femagtools import amela
-import json 
-import os
+import json
 import sys
+import os
+lsfrom femagtools import amela
 
-def read_output(): 
-    with open('tests/data/amela.out', 'r') as f: 
+
+def read_output():
+    with open('tests/data/amela.out', 'r') as f:
         data = f.readline()
     return data.split('\n')[0]
 
-def read_json(): 
-    with open('tests/data/pm_data/pm_data_se38.json', 'r') as f: 
+
+def read_json():
+    with open('tests/data/pm_data/pm_data_se38.json', 'r') as f:
         data = json.load(f)
-    return data 
+    return data
+
 
 def test_amela():
-    if sys.platform == 'linux':
-        os.chmod('tests/data/AMELA', 0o0777)
-    al = amela.Amela(workdir='tests/data', 
-                    magnet_data=dict(name='pm_data'))
+    al = amela.Amela(workdir='tests/data',
+                     magnet_data=dict(name='pm_data'))
     loss = al()
     r = read_json()
     assert r['name'] == 'pm_data_se38'
-    assert len(r['phi']) == 73  
+    assert len(r['phi']) == 73
     assert read_output() == 'succeed'
-    assert round(loss['pm_data_se38']['total_loss'], 3) == 0.881                                             
-
-    
+    assert round(loss['pm_data_se38']['total_loss'], 3) == 0.881

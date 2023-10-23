b955b17e5899f9e900e3fa867376483e1acb5e6f
Ronald Tanner <tar@semafor.ch>
2023-10-23 10:05:05 +0200

    added import pmrelsim, cogging, ..

--- src/femagtools/plot/__init__.py
+++ src/femagtools/plot/__init__.py
@@ -2,10 +2,12 @@
 
 """
 from .fluxdens import airgap, airgap_fft
-from .bch import torque, torque_fft, force, force_fft,\
+from .bch import torque, torque_fft, force, force_fft, \
     fluxdens_surface, winding_current, winding_flux, \
-    voltage, voltage_fft, transientsc_demag, \
-    i1beta_torque, i1beta_ld, i1beta_lq, i1beta_psid, i1beta_psiq, i1beta_psim, i1beta_up, \
+    voltage, voltage_fft, \
+    pmrelsim, multcal, fasttorque, cogging, transientsc, \
+    transientsc_demag, i1beta_torque, i1beta_ld, i1beta_lq, i1beta_psid, \
+    i1beta_psiq, i1beta_psim, i1beta_up, \
     idq_ld, idq_lq, idq_psid, idq_psim, idq_psiq, idq_torque
 from .char import mtpa, mtpv, characteristics, efficiency_map, losses_map
 from .forcedens import forcedens, forcedens_surface, forcedens_fft

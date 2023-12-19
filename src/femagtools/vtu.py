3d08e3fb82e8c31d79727b112bd43790d89712b8
Ronald Tanner <tar@semafor.ch>
2023-12-19 15:15:14 +0100

    get demag from vtu by magnet

--- src/femagtools/vtu.py
+++ src/femagtools/vtu.py
@@ -1,9 +1,11 @@
 """Read FEMAG vtu files
 
 """
-import vtk
+import logging
 import pathlib
 import numpy as np
+import vtk
+from vtkmodules.util.numpy_support import vtk_to_numpy
 
 
 class Reader(object):
@@ -23,11 +25,13 @@ class Reader(object):
         self.field_data_names = []
         self.point_data_names = []
         self.cell_data_names = []
+        assert pathlib.Path(pathname).exists(), f"{pathname} not found"
         if pathlib.Path(pathname).suffix == '.vtu':
             self.filenames = [pathlib.Path(pathname)]
         else:
             self.filenames = sorted(pathlib.Path(pathname).glob(
                 "*.vtu"))
+
         self.reader.SetFileName(str(self.filenames[0]))
         self.reader.Update()
         self.field_data_names = [
@@ -181,7 +185,7 @@ class Reader(object):
             point_vec.append(self.data[pnt_data][i].GetValue(pnt-1))
         return point_vec
 
-    def get_cell_vector(self, cell_data, cell) -> list:
+    def get_cell_vector(self, cell_data, cell=0) -> list:
         '''Read cell data
 
         Args:
@@ -195,6 +199,9 @@ class Reader(object):
         if cell_data not in self.data:
             self.read_data([cell_data])
 
+        if cell<=0:
+            return vtk_to_numpy(
+                self.output.GetCellData().GetAbstractArray(cell_data))
         i = self.cell_data_names.index(cell_data)
         noc = self.output.GetCellData().GetAbstractArray(i).GetNumberOfComponents()
         if noc == 1:
@@ -228,12 +235,64 @@ class Reader(object):
         else:
             return [cell_vec_x, cell_vec_y, cell_vec_z]
 
+    def hrphi(self, scf, elements, magtemp):
+        """return H values for each element in polar coord system
+        (experimental)
+        Args:
+          elements: list of model elements
+          magtemp: magnet temperature in degree Celsius
+          scf: scale factor (poles/poles in model)
+        """
+        MUE0 = 4e-7*np.pi
+        b = vtk_to_numpy(
+            self.output.GetCellData().GetAbstractArray("b"))[:, :2]
+        e = elements[0]
+        x, y = np.mean(vtk_to_numpy(
+            self.output.GetCell(e.key-1).GetPoints().GetData()), axis=0)[:2]
+        rotpos = np.arctan2(y, x) - np.arctan2(e.center[1], e.center[0])
+        psign = 1 if rotpos / scf > 1 else -1
+
+        h = np.empty((len(elements), 2))
+        for i, e in enumerate(elements):
+            x, y = e.center
+            alfa = np.arctan2(y, x)
+            b1, b2 = b[e.key-1]
+            btempc = 1. + e.br_temp_coef*(magtemp - 20.)
+            m1 = e.mag[0]*np.cos(alfa) + e.mag[1]*np.sin(alfa)
+            m2 = -e.mag[0]*np.sin(alfa) + e.mag[1]*np.cos(alfa)
+            h[i] = abs(e.reluc[0])/MUE0*np.array((
+                (b1 - psign*btempc*m1), (b2 - psign*btempc*m2)))
+        logging.info("H shape %s", h.shape)
+        return h
+
+
+    def demag(self, elements):
+        """return demag values for each element in kA/m
+        Args:
+          elements: list of model elements
+        Returns:
+          list of demags from each vtu file in set
+        """
+        dlist = []
+        for filename in self.filenames:
+            self.reader.SetFileName(str(filename))
+            self.reader.Update()
+            d = vtk_to_numpy(
+                self.output.GetCellData().GetAbstractArray("demagnetization"))
+            dm = np.empty((len(elements)))
+            for i, e in enumerate(elements):
+                dm[i] = d[e.key-1]
+            dlist.append(dm)
+        logging.info("Demag shape %s", np.array(dlist).shape)
+        return dlist
+
+
     def get_data_vector(self, data_name, key=0) -> list:
         '''Read data of fiels, point or cell
 
         Args:
           data_name : str Name of data to read
-        hey : int (optional) Key of point or cell
+          key : int (optional) Key of point or cell
 
         Returns:
             List of values within the time window
@@ -245,6 +304,16 @@ class Reader(object):
         if data_name in self.cell_data_names:
             return self.get_cell_vector(data_name, key)
         return []
+
+    def __repr__(self):
+        fmt = [f"N Cells {self.output.GetNumberOfCells()}  N Points {self.output.GetNumberOfPoints()}"]
+        fmt += [
+            f"{self.output.GetPointData().GetAbstractArray(i).GetName()}   Points"
+            for i in range(self.output.GetPointData().GetNumberOfArrays())]
+        fmt += [
+            f"{self.output.GetCellData().GetAbstractArray(i).GetName()}   Cells"
+            for i in range(self.output.GetCellData().GetNumberOfArrays())]
+        return '\n'.join(fmt)
 
 
 def read(filename) -> Reader:
--- src/tests/test_vtu.py
+++ src/tests/test_vtu.py
@@ -1,14 +1,19 @@
 import pytest
+import pathlib
 from femagtools import vtu
 
 
 @pytest.fixture
-def vtu_data():
-    filename = 'src/tests/data/zzz_pm_model_ts_results_1/zzz_pm_model_ts_0000.vtu'
-    return vtu.read(filename)
+def ts_data_dir():
+    return pathlib.Path(__file__).with_name('data') / 'zzz_pm_model_ts_results_1/'
+
+@pytest.fixture
+def demag_data_dir():
+    return pathlib.Path(__file__).with_name('data') / 'demag-vtu/'
 
 
-def test_read(vtu_data):
+def test_read(ts_data_dir):
+    vtu_data = vtu.read(ts_data_dir / 'zzz_pm_model_ts_0000.vtu')
     assert vtu_data.field_data_names == [
         'time [s]', 'angle [rad]',
         'speed [rad/s]', 'torque [Nm]',
@@ -23,3 +28,15 @@ def test_read(vtu_data):
     assert b[0] == pytest.approx([-0.003114], abs=1e-5)
     assert b[1] == pytest.approx([-0.00313], abs=1e-5)
     assert b[2] == [0.0]
+
+def test_demag(demag_data_dir):
+    vtu_data = vtu.read(demag_data_dir / 'PM_130_L10_0000.vtu')
+    keys = [7412, 7413, 7414, 7415, 7416]
+    class Element:
+        def __init__(self, key):
+            self.key = key
+    elements = [Element(k) for k in keys]
+    expected = [241.5, 250.2, 262.3, 276.5, 390.5]
+    actual = vtu_data.demag(elements)
+    assert len(actual) == 1
+    assert actual[0] == pytest.approx(expected, abs=0.1)

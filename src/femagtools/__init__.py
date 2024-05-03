437e42d5c6f1b75bd877c8f8e9b6a93183b9b051
Ronald Tanner <tar@semafor.ch>
2024-05-03 13:02:12 +0200

    prep rel 1.6.8

--- src/femagtools/__init__.py
+++ src/femagtools/__init__.py
@@ -2,7 +2,7 @@
 
 """
 __title__ = 'femagtools'
-__version__ = '1.6.8dev'
+__version__ = '1.6.8'
 __author__ = 'Ronald Tanner'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2016-2022 SEMAFOR Informatik & Energie AG'

2f0b4f5e346a3128890d956aac2b4b7170bb7d1a
Ronald Tanner <tar@semafor.ch>
2024-08-13 14:33:07 +0200

    started new dev cycle

--- src/femagtools/__init__.py
+++ src/femagtools/__init__.py
@@ -2,7 +2,7 @@
 
 """
 __title__ = 'femagtools'
-__version__ = '1.7.5'
+__version__ = '1.7.6dev'
 __author__ = 'Ronald Tanner'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2023-2024 Gamma Technology'

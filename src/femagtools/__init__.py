f364a70195d81740edd226e980d7ae01deed8c5e
Ronald Tanner <tar@semafor.ch>
2025-01-09 14:01:16 +0100

    prep rel 1.8.3

--- src/femagtools/__init__.py
+++ src/femagtools/__init__.py
@@ -2,7 +2,7 @@
 
 """
 __title__ = 'femagtools'
-__version__ = '1.8.3dev'
+__version__ = '1.8.3'
 __author__ = 'Ronald Tanner'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2023-2024 Gamma Technology'

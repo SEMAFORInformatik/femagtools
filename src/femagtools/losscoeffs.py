931f5b0ddc755ab3d9fe17c4f078513478cfdbdf
dapu <dzhang@gtisoft.com>
2025-02-12 13:28:22 +0100

    add loss fit for modified steinmetz model

--- src/femagtools/losscoeffs.py
+++ src/femagtools/losscoeffs.py
@@ -20,6 +20,8 @@ def pfe_jordan(f, B, ch, fh, cw, fw, fb, fo, Bo):
 def pfe_steinmetz(f, B, cw, fw, fb, fo, Bo):
     return cw*(f/fo)**fw * (B/Bo)**fb
 
+def pfe_modified_steinmetz(f, B, ch, cw, alpha, beta, fo, Bo):
+    return ch*(f/fo)*(B/Bo)**(alpha + B*beta) + cw*(f/fo)**2*(B/Bo)**2
 
 def fitsteinmetz(f, B, losses, Bo, fo, alpha0=1.0):
     """fit coeffs of
@@ -56,6 +58,30 @@ def fitsteinmetz(f, B, losses, Bo, fo, alpha0=1.0):
             fbx, y, (1.0, 1.0, 2.0))
     return fitp
 
+def fit_modified_steinmetz(f, B, losses, Bo, fo):
+    """fit coeffs of modifeld steinmetz
+    losses(f,B)=ch*(f/fo)*(B/Bo)**(alpha + B*beta) + cw*(f/fo)**2*(B/Bo)**2
+    returns (ch, cw, alpha, beta)
+    """
+    pfe = losses
+    z = []
+    for i, fx in enumerate(f):
+        if fx:
+            if isinstance(B[0], float):
+                z += [(fx, bx, y)
+                      for bx, y in zip(B, pfe[i])
+                      if y]
+            else:
+                z += [(fx, bx, y)
+                      for bx, y in zip(B[i], pfe[i])
+                      if y]
+
+    fbx = np.array(z).T[0:2]
+    y = np.array(z).T[2]
+    fitp, cov = so.curve_fit(lambda x, ch, cw, alpha, beta: pfe_modified_steinmetz(
+        x[0], x[1], ch, cw, alpha, beta, fo, Bo),
+        fbx, y, (1.0, 1.0, 1.0, 1.0))
+    return fitp
 
 def fitjordan(f, B, losses, Bo, fo):
     """fit coeffs of
